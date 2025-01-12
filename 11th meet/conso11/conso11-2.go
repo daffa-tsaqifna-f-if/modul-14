e66586c18ac5bf864b1286ca157edb8b6a201f4b
daffa-tsaqifna-f-if <theminerz04@gmail.com>
2025-01-12 09:20:21 +0700

    conso11-2.go

--- 11th meet/conso11/conso11-2.go
+++ 11th meet/conso11/conso11-2.go
@@ -1,12 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	var b1, b2, b3, max, min int 
-    fmt.Scan(&b1, &b2, &b3) 
-    if b1 > b2 { 
-        max = b1 
-        min = b2 
-    }else { 
-        max = b2 
-        min = b1
+	var b1, b2, b3, max, min int
+	fmt.Scan(&b1, &b2, &b3)
+	if b1 > b2 {
+		max = b1
+		min = b2
+	} else {
+		max = b2
+		min = b1
+	}
 }
